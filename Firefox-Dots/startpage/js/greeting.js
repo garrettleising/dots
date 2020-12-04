eb401d9f3eb79ef9687b02e0dc28a8324ef73a57
Garrett <gzleising@gmail.com>
2020-12-03 22:45:40 -0800

    Changed styling of front page, not using bento box

---
+++ Firefox-Dots/startpage/js/greeting.js
@@ -0,0 +1,25 @@
+// Get the hour
+var today = new Date();
+var hour = today.getHours();
+
+// Here you can change your name
+var name = "Garrett Leising";
+
+// Here you can change your greetings
+var gree1 = "Go to sleep! ";
+var gree2 = "Good Morning! ";
+var gree3 = "Good afternoon ";
+var gree4 = "Good evening ";
+var gree5 = "Good evening ";
+var gree6 = "Good evening ";
+
+// Define the hours of the greetings
+if (hour >= 23 && hour < 5) {
+  document.getElementById("greetings").innerText = gree1 + name;
+} else if (hour >= 6 && hour < 12) {
+  document.getElementById("greetings").innerText = gree2 + name;
+} else if (hour >= 12 && hour < 17) {
+  document.getElementById("greetings").innerText = gree3 + name;
+} else {
+  document.getElementById("greetings").innerText = gree4 + name;
+}

70a49e6a87ed20ce11bde9e1d567c5926d62ffe4
Garrett <gzleising@gmail.com>
2020-12-05 03:54:40 -0800

    formatting JS files

--- Firefox-Dots/startpage/js/greeting.js
+++ Firefox-Dots/startpage/js/greeting.js
@@ -14,7 +14,7 @@ var gree5 = "Good evening ";
 var gree6 = "Good evening ";
 
 // Define the hours of the greetings
-if (hour >= 23 && hour < 5) {
+if (hour >= 23 || hour < 5) {
   document.getElementById("greetings").innerText = gree1 + name;
 } else if (hour >= 6 && hour < 12) {
   document.getElementById("greetings").innerText = gree2 + name;
